f84ad89cb292a7865792993ed41649c37a7a47a3
Paul Rondeau <paulrondeau07@gmail.com>
2013-11-08 17:13:03 -0500

    files changed

--- Login/login.py
+++ Login/login.py
@@ -7,6 +7,7 @@ from StockPricesDB import Stocks
 
 class LoginUser(handle.Handler):
     def get(self):
+        # Stocks.insertStockStringInDataBase("{'aapl': {'price': '188', 'time': '10-5-6'}}")
         self.render("loginPage.html")
     def post(self):
         username = self.request.get('username')
