41c00cbc76fb00457f3dee01b17343a5bac177f3
Shayan Arman <shayan.arman2@gmail.com>
2013-12-22 20:17:50 -0700

    Added more functionality to the sellPage. The user can now see the number of shares they have for each stock

--- js/sellStocksHandler.js
+++ js/sellStocksHandler.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
+	$.getJSON("/renderUserStocksDB").complete(function(data) {
+		var jstring = data.responseText;
+		if(jstring != 'Empty') {
+			var jarray = JSON.parse(jstring);
+			var color = 'red';
+			for (var i=0;i<jarray.length;i++) { 
+				companyTicker = jarray[i].ticker;
+				numberShares  = jarray[i].numberOfShares;
+				currentPrice  = jarray[i].currentPrice;
+				marketValue   = currentPrice*numberShares;
+				marketValue   = Math.round(marketValue * 100)/100;
+				originalPrice = jarray[i].boughtAt;
+				bookValue     = numberShares*originalPrice;
+				bookValue     = Math.round(bookValue * 100)/100;
+
+				moneyMade 	  = marketValue - bookValue;
+				percentGain   = (((marketValue/bookValue)-1)*100).toFixed(2);
+
+
+				if(currentPrice>originalPrice) {
+						color = '#008000';
+				} else {
+					color = 'red';
+				}
+				var tableRow = makeTableRows(companyTicker,moneyMade,percentGain,currentPrice,numberShares,color);
+				$('#tableBody').append(tableRow);
+			}
+		}
+	});
+
 	$("#sellstocksbutton").click(function() {
 		$('#sellstocksbutton').attr('disabled', 'disabled');
 		var ticker = $("#tickerSell").val();
@@ -30,6 +60,15 @@ $(document).ready(function() {
 	        x = Number(x);
 	        return x === Math.floor(x) ? x : 'wrong';
 	    }
+	}
+
+	function makeTableRows(tick,moneyMade,percentGain,currentPrice,numberShares,color) {
+		var row = "<tr><td style=\"color:"+color+";font-weight:bold\">"+tick+"</td>"+
+							"<td>"+moneyMade+"</td>"+
+							"<td style=\"color:"+color+";font-weight:bold\">"+percentGain+" %</td>"+
+							"<td>"+currentPrice+"</td>"+
+							"<td style=\"color:"+color+";font-weight:bold\">"+numberShares+"</td></tr>";
+		return row;
 	}
 
 });
